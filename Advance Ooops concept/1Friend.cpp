82c3219419e6f75d2a4b6844aba34e6c8b084506
Mahapurush <mahapurush@rts.in>
2021-06-17 13:49:32 +0530

    Updated

--- Advance Ooops concept/1Friend.cpp
+++ Advance Ooops concept/1Friend.cpp
@@ -17,8 +17,12 @@ void fun()
     Test T;
     T.a = 10;
     T.b = 20;
-    T.c = 30;
+    T.c = 30; // By Default an obecj created in function which is not in inherited class
+    // will not have access of private and protected data
+    //using friend function we can give access.
+    cout << T.a << " " << T.b << " " << T.c << endl;
 }
 int main()
 {
+    fun();
 }
